a49d3934c78d0e562932b86f2f858a94a0838bfc
vtphatt2 <thinhphat544@gmail.com>
2024-06-27 21:29:22 +0700

    add search bar and booking services

--- app/src/main/java/com/example/travelly/MainActivity.java
+++ app/src/main/java/com/example/travelly/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.travelly;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout llTrips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        llTrips = findViewById(R.id.llTrips);
+        llTrips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Success", "llTrips : On click");
+            }
         });
     }
 }
