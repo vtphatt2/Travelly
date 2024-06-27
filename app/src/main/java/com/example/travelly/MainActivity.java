f88f8435dc9b84b2a9d80d1f55b0c545acc0c841
vtphatt2 <thinhphat544@gmail.com>
2024-06-27 21:47:15 +0700

    add listener for each services

--- app/src/main/java/com/example/travelly/MainActivity.java
+++ app/src/main/java/com/example/travelly/MainActivity.java
@@ -6,13 +6,14 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout llTrips;
+    private LinearLayout llTrips, llHotel, llTransport, llEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +27,46 @@ public class MainActivity extends AppCompatActivity {
         });
 
         llTrips = findViewById(R.id.llTrips);
+        llHotel = findViewById(R.id.llHotel);
+        llTransport = findViewById(R.id.llTransport);
+        llEvents = findViewById(R.id.llEvents);
         llTrips.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Success", "llTrips : On click");
+                showDialog("Trips");
             }
         });
+
+        llHotel .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Success", "llHotel  : On click");
+                showDialog("Hotel");
+            }
+        });
+
+        llEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Success", "llEvents : On click");
+                showDialog("Events");
+            }
+        });
+
+        llTransport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Success", "llTransport : On click");
+            }
+        });
+    }
+
+    private void showDialog(String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(title + " services are not available now.");
+        builder.setPositiveButton("OK", null);
+        builder.show();
     }
 }
