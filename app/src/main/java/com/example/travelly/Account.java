93e28fc589117f76d8754c30f8973d8b6f9d81ba
vtphatt2 <thinhphat544@gmail.com>
2024-06-28 16:08:15 +0700

    finish Account fragment

--- app/src/main/java/com/example/travelly/Account.java
+++ app/src/main/java/com/example/travelly/Account.java
@@ -9,10 +9,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class Account extends Fragment {
-    private LinearLayout llPayment;
+    private LinearLayout llPayment, llSaved, llBooking, llSetting;
+    private Button btnEndSession;
 
     public Account() {
         // Required empty public constructor
@@ -34,12 +36,48 @@ public class Account extends Fragment {
         View view = inflater.inflate(R.layout.fragment_account, container, false);
 
         llPayment = view.findViewById(R.id.llPayment);
+        llSaved = view.findViewById(R.id.llSaved);
+        llBooking = view.findViewById(R.id.llBooking);
+        llSetting = view.findViewById(R.id.llSetting);
+
 
         llPayment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDialog("Payment and cards");
                 Log.d("Success", "Payment and cards : On click");
+            }
+        });
+
+        llSaved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog("Saved");
+                Log.d("Success", "Saved : On click");
+            }
+        });
+
+        llBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog("Booking history");
+                Log.d("Success", "Booking history : On click");
+            }
+        });
+
+        llSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog("Settings");
+                Log.d("Success", "Settings");
+            }
+        });
+
+        btnEndSession = view.findViewById(R.id.endSessionButton);
+        btnEndSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showExitConfirmationDialog();
             }
         });
 
@@ -52,5 +90,17 @@ public class Account extends Fragment {
         builder.setMessage(title + " function is being developed...");
         builder.setPositiveButton("OK", null);
         builder.show();
+    }
+
+    private void showExitConfirmationDialog() {
+        new AlertDialog.Builder(requireActivity())
+                .setTitle("Confirm Exit")
+                .setMessage("Are you sure you want to close the app?")
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                    requireActivity().finish();
+                    System.exit(0);
+                }).setNegativeButton(android.R.string.cancel, (dialog, which) -> {
+                    dialog.dismiss();
+                }).show();
     }
 }
